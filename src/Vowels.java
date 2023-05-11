10048eebebe627964295678176aa1231c50e7320
rajesh268 <rajeshravi268@gmail.com>
2023-05-11 07:36:39 +0530

    first commit

---
+++ src/Vowels.java
@@ -0,0 +1,16 @@
+
+public class Vowels {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         char x='a';
+        
+        if (x=='a'||x=='e'||x=='i'||x=='o'||x=='u') {
+            System.out.println(x + " is a vowel");
+        } else {
+            System.out.println(x + " is a consonant");
+        }
+    }
+	}
+
+
